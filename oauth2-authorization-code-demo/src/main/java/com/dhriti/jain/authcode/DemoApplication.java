448c64943ff0772376c753379ba5f9381933fa98
Dell Pc <jaindhrit12@gmail.com>
2023-11-17 13:06:11 +0530

    initial spring security keycload project

---
+++ oauth2-authorization-code-demo/src/main/java/com/dhriti/jain/authcode/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.dhriti.jain.authcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
